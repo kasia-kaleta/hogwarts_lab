28f9ca3a11aefc56b116479a2fe93d25bb36c1fa
kat3005 <kasia.alicja@gmail.com>
2019-07-18 11:45:35 +0100

    Adds student1 object

--- models/student.rb
+++ models/student.rb
@@ -1,3 +1,5 @@
+require_relative('../db/sql_runner.rb')
+
 class Student
 
   attr_reader :id
@@ -11,4 +13,17 @@ class Student
     @age = options['age'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO students (first_name, last_name, house, age) VALUES ($1, $2, $3, $4) RETURNING id"
+    values = [@first_name, @last_name, @house, @age]
+    @id = SqlRunner.run(sql, values).first['id'].to_i
+  end
+
+
+  def self.delete_all()
+    sql = "DELETE FROM students"
+    SqlRunner.run(sql)
+  end
+
+  
 end
