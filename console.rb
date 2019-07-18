e9c9f782f02508599890717d8fdb617901f3193d
kat3005 <kasia.alicja@gmail.com>
2019-07-18 12:46:28 +0100

    Find methods complete

--- console.rb
+++ console.rb
@@ -2,16 +2,6 @@ require_relative('./models/student')
 require('pry')
 
 
-Student.delete_all()
-
-student1 = Student.new({
-  'first_name' => 'Harry',
-  'last_name' => 'Potter',
-  'house' => 'Gryffindor',
-  'age' => 13
-  })
-  student1.save()
-
 
 
 
--- models/student.rb
+++ models/student.rb
@@ -19,11 +19,21 @@ class Student
     @id = SqlRunner.run(sql, values).first['id'].to_i
   end
 
+def self.find(id)
+  sql ="SELECT * FROM students WHERE ID = $1"
+  values=[id]
+  return SqlRunner.run(sql,values).first
+end
+
+def self.find_all
+  sql ="SELECT * FROM students"
+  return SqlRunner.run(sql)
+end
 
   def self.delete_all()
     sql = "DELETE FROM students"
     SqlRunner.run(sql)
   end
 
-  
+
 end
